6b1cf3e420abfdf27d76f5d559b97cb14b87e7c9
mharrigan26 <39278650+mharrigan26@users.noreply.github.com>
2018-05-20 19:45:17 -0400

    Add files via upload

--- Hangman.java
+++ Hangman.java
@@ -16,8 +16,8 @@ public class Hangman
     public Hangman(String category)
     {
         // initialise instance variables
-        //Scanner reader = new Scanner(new File(category));
-
+        Scanner reader = new Scanner(new File(category));
+        
     }
 
     public Hangman(String player1, String player2){
@@ -43,9 +43,9 @@ public class Hangman
             rounds++;
         }
     }
-
+    
     public static void main (String[] args){
         Hangman test = new Hangman("one", "two");
-
+        
     }
 }
