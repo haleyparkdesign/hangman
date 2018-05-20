c2e5ede3be24515ce791a1a75db63b258e614ecc
Margaret <mgharrigan26@gmail.com>
2018-05-20 19:49:57 -0400

    Revert "Add files via upload"
    This reverts commit 6b1cf3e420abfdf27d76f5d559b97cb14b87e7c9.

--- HangmanGUI.java
+++ HangmanGUI.java
@@ -1,26 +1,36 @@
 import javax.swing.JTabbedPane;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.*;
 
 /**
- * Write a description of class MultiPlayerGamePanel here.
  * GUI & driver for the whole program. 
  *
- * @author (your name)
- * @version (a version number or a date)
  * @author Haley Park (add your names as you modify the class!)
  * @version May 19, 2018
  */
 
 public class HangmanGUI extends JPanel {
+    public HangmanGUI() {
+        JTabbedPane hangmanGUI = new JTabbedPane();
+        JPanel singlePlayerTab = new SinglePlayerPanel();
+        hangmanGUI.addTab("Single Player", singlePlayerTab);
+        hangmanGUI.setSelectedIndex(0);
+        JPanel multiPlayerTab = new MultiPlayerPanel();
+        hangmanGUI.addTab("Multiplayer", multiPlayerTab);
+
+        // Add the tabbed pane to this panel.
+        setLayout(new GridLayout(1, 1));
+        add(hangmanGUI);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hangman");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.getContentPane().add(new HangmanGUI(),BorderLayout.CENTER);
-        frame.setSize(1000, 600);
         frame.setSize(800, 400);
         frame.setVisible(true);
+    }
 }
-
