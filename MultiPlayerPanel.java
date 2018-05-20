aef7d559612bc242379379beda857f6f2bf79f1c
deezusdyse <dchai@wellesley.edu>
2018-05-20 13:43:55 -0400

    multiplayer update, still working

--- MultiPlayerPanel.java
+++ MultiPlayerPanel.java
@@ -1,16 +1,383 @@
-import javax.swing.JTabbedPane;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
 import java.awt.event.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.util.Vector;
+import java.util.LinkedList;
 
 /**
- * Write a description of class MultiPlayerGamePanel here.
+ * GUI elements for single player option
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Denise Chai, Haley Park
+ * @version May 15, 2018
  */
-public class MultiPlayerPanel  extends JPanel {
+public class MultiPlayerPanel extends JPanel {
+    private JButton[] buttons;
+    private CardLayout cardLayout = new CardLayout();
+    private JPanel cardPanel,cardPanel2, enterLetterPanel,gamePlayPanel, enterWordPanel1, enterWordPanel2;
+    private JLabel guessLabel1, guessLabel2, wrongGuesses1, hintLabel1, hintLabel2, imageLabel1, imageLabel2;
+    private JTextField guessField1, guessField2;
+    private JTextField guessWord1;
+     private JTextField guessWord2;
+    private JTextField[] letters1,letters2;
+    private JButton hintButton1, skipButton1, hintButton2, skipButton2;
+    private MultiPlayerRound newRound;
+    private JLabel[] wrongGuessLabels1, wrongGuessLabels2;
 
+    public MultiPlayerPanel() {
+        
+        wrongGuessLabels1 = new JLabel[9];
+        wrongGuessLabels2 = new JLabel[9];
+        
+        enterWordPanel1 = makeEnterWordPanel1();
+        enterWordPanel2 = makeEnterWordPanel2();
+        //gamePlayPanel = makeMultiPlayerGamePanel();
+       
+        cardPanel = new JPanel();
+        cardPanel.setLayout(cardLayout);
+        cardPanel.add(enterWordPanel1, "1");
+        
+        cardPanel.add(enterWordPanel2, "2");
+        
+        add(cardPanel);
+      
+
+        // initialize with showing the categories panel
+        cardLayout.first(cardPanel);
+       
+    }
+
+    private JPanel makeEnterWordPanel1() {
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new GridLayout(5, 3, 10, 10));
+
+        JLabel statusLabel = new JLabel("Player 1's turn to enter the word", JLabel.CENTER);
+        guessWord1 = new JTextField();
+        guessWord1.addActionListener(new TextFieldListener());
+        panel.add(statusLabel);   
+        panel.add(guessWord1);
+        return panel;
+    }
+    
+    
+    private JPanel makeEnterWordPanel2() {
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new GridLayout(5, 3, 10, 10));
+
+        JLabel statusLabel = new JLabel("Player 2's turn to enter the word", JLabel.CENTER);
+        guessWord2 = new JTextField();
+        guessWord2.addActionListener(new TextFieldListener());
+        panel.add(statusLabel);
+        panel.add(guessWord2);
+        
+        return panel;
+    }
+    
+
+    private JPanel makeMultiPlayerGamePanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        
+        //PANEL 1
+        int numTextFields1 = newRound.getWord1().length();
+   
+        JPanel topPanel1 = new JPanel();
+        JPanel lettersArea1 = new JPanel();
+        JPanel letterBoxesArea1 = new JPanel();
+        JPanel wrongGuessesArea1 = new JPanel();
+        JPanel bottomPanel1 = new JPanel();
+
+        JPanel guessPanel1 = new JPanel();
+        guessLabel1 = new JLabel("Guess! ");
+        guessField1 = new JTextField(5);
+        guessField1.addActionListener(new TextFieldListener());
+        guessPanel1.add(guessLabel1);
+        guessPanel1.add(guessField1);
+
+        // topPanel.add(categoryLabel);
+        topPanel1.add(guessPanel1);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.FIRST_LINE_END;
+        panel.add(guessPanel1, c);
+
+        // Add the hangman image
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        
+        try {
+            BufferedImage image1 = ImageIO.read(new File("./images/0.png"));
+            imageLabel1 = new JLabel(new ImageIcon(image1));
+            panel.add(imageLabel1, c);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        
+        /////
+        // Add guess boxes
+        lettersArea1.setLayout(new BorderLayout(10, 10));
+        letters1 = new JTextField[numTextFields1];
+        for (int i = 0; i < numTextFields1; i++) {
+            letters1[i] = new JTextField("  ");
+            letters1[i].setHorizontalAlignment(JTextField.CENTER);
+            letters1[i].setEnabled(false);
+            letters1[i].setPreferredSize(new Dimension(20, 40));
+            letters1[i].setFont(new Font("Sans-Serif", Font.BOLD, 20));
+            letterBoxesArea1.add(letters1[i]);
+        }
+        lettersArea1.add(letterBoxesArea1, BorderLayout.NORTH);
+
+        for (int i = 0; i < wrongGuessLabels1.length; i++) {
+            wrongGuessLabels1[i] = new JLabel(" ");
+            wrongGuessesArea1.add(wrongGuessLabels1[i]);
+        }
+        lettersArea1.add(wrongGuessesArea1, BorderLayout.SOUTH);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 1;
+        panel.add(lettersArea1, c);
+
+        // Add buttons
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 3;
+        hintButton1 = new JButton("Hint");
+        hintButton1.addActionListener(new ButtonListener());
+        panel.add(hintButton1, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 3;
+        hintLabel1 = new JLabel();
+        panel.add(hintLabel1, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 3;
+        c.gridy = 3;
+        skipButton1 = new JButton("Skip");
+        skipButton1.addActionListener(new ButtonListener());
+        panel.add(skipButton1, c);
+        
+        
+        
+        //PANEL 2
+        int numTextFields2 = newRound.getWord2().length();
+   
+        JPanel topPanel2 = new JPanel();
+        JPanel lettersArea2 = new JPanel();
+        JPanel letterBoxesArea2 = new JPanel();
+        JPanel wrongGuessesArea2 = new JPanel();
+        JPanel bottomPanel2 = new JPanel();
+
+        JPanel guessPanel2 = new JPanel();
+        guessLabel2 = new JLabel("Guess! ");
+        guessField2 = new JTextField(5);
+        guessField2.addActionListener(new TextFieldListener());
+        guessPanel2.add(guessLabel2);
+        guessPanel2.add(guessField2);
+
+        // topPanel.add(categoryLabel);
+        topPanel2.add(guessPanel2);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.FIRST_LINE_END;
+        panel.add(guessPanel2, c);
+
+        // Add the hangman image
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+
+        try {
+            BufferedImage image2 = ImageIO.read(new File("./images/0.png"));
+            imageLabel2 = new JLabel(new ImageIcon(image2));
+            panel.add(imageLabel2, c);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        // Add guess boxes
+        lettersArea2.setLayout(new BorderLayout(10, 10));
+        letters2 = new JTextField[numTextFields2];
+        for (int i = 0; i < numTextFields2; i++) {
+            letters2[i] = new JTextField("  ");
+            letters2[i].setHorizontalAlignment(JTextField.CENTER);
+            letters2[i].setEnabled(false);
+            letters2[i].setPreferredSize(new Dimension(20, 40));
+            letters2[i].setFont(new Font("Sans-Serif", Font.BOLD, 20));
+            letterBoxesArea2.add(letters2[i]);
+        }
+        lettersArea2.add(letterBoxesArea2, BorderLayout.NORTH);
+
+        for (int i = 0; i < wrongGuessLabels2.length; i++) {
+            wrongGuessLabels2[i] = new JLabel(" ");
+            wrongGuessesArea2.add(wrongGuessLabels2[i]);
+        }
+        lettersArea2.add(wrongGuessesArea2, BorderLayout.SOUTH);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 1;
+        panel.add(lettersArea2, c);
+
+        // Add buttons
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 3;
+        hintButton2 = new JButton("Hint");
+        hintButton2.addActionListener(new ButtonListener());
+        panel.add(hintButton2, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 3;
+        hintLabel2 = new JLabel();
+        panel.add(hintLabel2, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 3;
+        c.gridy = 3;
+        skipButton2 = new JButton("Skip");
+        skipButton2.addActionListener(new ButtonListener());
+        panel.add(skipButton2, c);
+
+
+        return panel;
+    }
+
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed (ActionEvent event) {
+            if (event.getSource() == hintButton1 || event.getSource() == hintButton2) {
+                if (event.getSource() == hintButton1) {
+                    hintLabel1.setText(newRound.getHint1());
+                }
+                else {
+                    hintLabel2.setText(newRound.getHint2());
+                }
+            } else if (event.getSource() == skipButton1 || event.getSource() == skipButton2) {
+                //hide the game panel, show categories panel
+                cardLayout.first(cardPanel);
+                //gamePlayPanel = makeSinglePlayerGamePanel();
+                resetGame();
+            } else {
+         
+                newRound = new MultiPlayerRound();
+
+                gamePlayPanel = makeMultiPlayerGamePanel();
+    
+                cardPanel.add(gamePlayPanel, "3");
+          
+
+                //hide the categories panel, show game panel
+                cardLayout.last(cardPanel);
+            }
+        }
+
+        private void resetGame() {
+            cardPanel.removeAll();
+  
+            gamePlayPanel = makeMultiPlayerGamePanel();
+ 
+            cardPanel = new JPanel();
+            cardPanel.setLayout(cardLayout);
+
+            cardPanel.add(gamePlayPanel, "3");
+
+
+            add(cardPanel);
+
+            // initialize with showing the categories panel
+            cardLayout.first(cardPanel);
+        }
+    }
+
+    private class TextFieldListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            char guessedChar1;
+            char guessedChar2;
+
+            LinkedList<Character> wrongGuesses1 = newRound.getWrongGuesses1();
+            LinkedList<Character> wrongGuesses2 = newRound.getWrongGuesses2();
+            
+            
+            if (e.getSource() == guessWord1) {
+                newRound.word1= guessWord1.getText();
+                cardLayout.next(cardPanel);             
+            }
+            
+            else if (e.getSource() == guessWord2) {
+                newRound.word2= guessWord2.getText();
+                cardLayout.next(cardPanel);
+            }
+            
+            
+            if (e.getSource() == guessField1 && newRound.currentPlayer % 2 == 0) {
+                String input = guessField1.getText();
+                System.out.println("Input: " + input);
+                guessedChar1 = input.charAt(0);
+    
+                newRound.makeGuess1(guessedChar1);
+                guessField1.setText("");
+                         
+    
+                for (int i = 0; i < wrongGuesses1.size(); i++) {
+                    wrongGuessLabels1[i].setText("" + wrongGuesses1.get(i));
+                }
+    
+                fillBlanks(guessedChar1);
+                setImage();
+            }
+            else if (e.getSource() == guessField2 && newRound.currentPlayer % 2 != 0) {
+                String input = guessField2.getText();
+                System.out.println("Input: " + input);
+                guessedChar2 = input.charAt(0);
+    
+                newRound.makeGuess2(guessedChar2);
+                guessField2.setText("");
+                         
+    
+                for (int i = 0; i < wrongGuesses2.size(); i++) {
+                    wrongGuessLabels2[i].setText("" + wrongGuesses2.get(i));
+                }
+    
+                fillBlanks(guessedChar2);
+                setImage();
+            }
+        }
+
+        private void fillBlanks(char guessedChar) {
+            Vector<Integer> whereToFill1 = newRound.letterFit1(guessedChar);
+            for (int i = 0; i < whereToFill1.size(); i++) {
+                letters1[whereToFill1.get(i)].setText("" + Character.toUpperCase(guessedChar));
+            }
+            Vector<Integer> whereToFill2 = newRound.letterFit2(guessedChar);
+            for (int i = 0; i < whereToFill2.size(); i++) {
+                letters2[whereToFill2.get(i)].setText("" + Character.toUpperCase(guessedChar));
+            }
+        }
+
+        private void setImage() {
+            int numWrongGuesses1 = newRound.getNumWrongGuesses1();
+            int numWrongGuesses2 = newRound.getNumWrongGuesses2();
+            try {
+                BufferedImage image1 = ImageIO.read(new File("./images/" + numWrongGuesses1 + ".png"));
+                imageLabel1.setIcon(new ImageIcon(image1));
+                BufferedImage image2 = ImageIO.read(new File("./images/" + numWrongGuesses2 + ".png"));
+                imageLabel2.setIcon(new ImageIcon(image2));
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
--- MultiPlayerRound.java
+++ MultiPlayerRound.java
@@ -9,7 +9,6 @@ public class MultiPlayerRound {
     private Vector<String> dictionary;
     private boolean player1Win;
     private boolean player2Win;
-    private String original;
     private HashSet word1Set;
     private HashSet word2Set;
     private LinkedList guessed1, guessed2, wrongGuesses1, wrongGuesses2;
@@ -18,6 +17,8 @@ public class MultiPlayerRound {
     String word1;
     String word2;
     int currentPlayer;
+    int count1 = 0;
+    int count2 = 0;
 
     /**
      * Constructor for objects of class SinglePlayerRound
@@ -49,91 +50,60 @@ public class MultiPlayerRound {
         
     }
 
-    public void makeGuess() {
-        int count1 = 0;
-        int count2 = 0;
-        Scanner scan = new Scanner(System.in); 
-        while (!player1Win && !player2Win) {
-            //player 1
-            if (currentPlayer % 2  == 0 ) {
-                currentPlayer ++;
-                System.out.println("Player 1: Enter a guess");
-                char letter = scan.next().charAt(0);
-                //if the user hasn't guessed this letter
-                if (!guessed1.contains(letter)) {
-                    if (word1Set.contains(letter)) {
-                        System.out.println(letter + " is in the word");
-                        count1++;
-                        guessed1.add(letter);
-                    } else {
-                        System.out.println(letter + " is not in the word :(");
-                        wrongGuesses1.add(letter);
-                        guessed1.add(letter);
-                    }
+    public void makeGuess1(char guess) {
+        if (!player1Win && !player2Win ){
+            if (!guessed1.contains(guess)) {
+                if (word1.indexOf(guess) > 0) {
+                    System.out.println(guess + " is in the word");
+                    count1++;
+                    guessed1.add(guess);
+                } else {
+                    System.out.println(guess + " is not in the word :(");
+                    count1++;
+                    wrongGuesses1.add(guess);
+                    guessed1.add(guess);
                 }
-                
-                else {
-                    System.out.println("You've already guessed this letter. Try again.");
-                    }
-                    
-                if (wrongGuesses1.size() == 9 ) {
-                    player1Win = false;
-                    player2Win = true;
-                }
-                if (count1 == word1Set.size()) {
-                    player1Win = true;
-                    player2Win = false;
-                }
-                
+            } else {
+                System.out.println("You've already guessed this letter. Try again.");
             }
-            else if (currentPlayer % 2  != 0 ) {
-                currentPlayer ++;
-                System.out.println("Player 2: Enter a guess");
-                char letter = scan.next().charAt(0);
-                //if the user hasn't guessed this letter
-                if (!guessed2.contains(letter)) {
-                    if (word2Set.contains(letter)) {
-                        System.out.println(letter + " is in the word");
-                        guessed2.add(letter);
-                        count2 ++;
-                    } else {
-                        System.out.println(letter + " is not in the word :(");
-                        wrongGuesses2.add(letter);
-                        guessed2.add(letter);
-                    }
+        } else {
+            System.out.println("The word was " + word1 + ". It took Player1 " + guessed1.size()
+                + " guesses. These are the letters you guessed: " + guessed1);
+        }
+    }
+    
+    public void makeGuess2(char guess) {
+        if (wrongGuesses2.size() <= 9 ){
+            if (!guessed2.contains(guess)) {
+                if (word2.indexOf(guess) > 0) {
+                    System.out.println(guess + " is in the word");
+                    count2++;
+                    guessed2.add(guess);
+                } else {
+                    System.out.println(guess + " is not in the word :(");
+                    wrongGuesses2.add(guess);
+                    count2++;
+                    guessed2.add(guess);
                 }
-                
-                else {
-                    System.out.println("You've already guessed this letter. Try again.");
-                    }
-                if (wrongGuesses2.size() == 9 ) {
-                    player1Win = true;
-                    player2Win = false;
-                }
-                if (count2 == word2Set.size()) {
-                    player1Win = false;
-                    player2Win = true;
-                }
-            }      
+            } else {
+                System.out.println("You've already guessed this letter. Try again.");
+            }
+        } else {
+            System.out.println("The word was " + word2 + ". It took Player2 " + guessed2.size()
+                + " guesses. These are the letters you guessed: " + guessed2);
         }
-        
-        if (player1Win) {
-            System.out.println("Player 1 correctly guessed the word " + word1 + ". It took Player1 " + guessed1.size() + " guesses. ");
-        }
-        else {
-            System.out.println("Player 2 correctly guessed the word " + word1 + ". It took Player2 " + guessed2.size() + " guesses. ");
-        }
-        }
-   
-    public String getHint() {
+    }
+    
+
+    public String getHint1() {
         String hintChar = "";
         if (!guessed1.contains(word1.charAt(0))) {
-            hintChar = original.substring(0,1);
+            hintChar = word1.substring(0,1);
             return "Starts with: " + hintChar;
         } else {
-            for (int i = 1; i < original.length(); i++) {
+            for (int i = 1; i < word1.length(); i++) {
                 if (!guessed1.contains(word1.charAt(i))) {
-                    hintChar = original.substring(i,i+1);
+                    hintChar = word1.substring(i,i+1);
                     break;
                 } 
             }
@@ -142,12 +112,69 @@ public class MultiPlayerRound {
         return "Contains letter " + hintChar;
     }
 
-    public String getWrongGuesses1() {
+    public Vector<Integer> letterFit1(char guess) {
+        Vector<Integer> correctLetterPos = new Vector<Integer>();
+        char[] charArray = word1.toCharArray();
+        
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == guess)
+                correctLetterPos.add(i);
+        }
+        
+        return correctLetterPos;
+    }
+    
+    
+    public Vector<Integer> letterFit2(char guess) {
+        Vector<Integer> correctLetterPos = new Vector<Integer>();
+        char[] charArray = word2.toCharArray();
+        
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == guess)
+                correctLetterPos.add(i);
+        }
+        
+        return correctLetterPos;
+    }
+    
+    public String getHint2() {
+        String hintChar = "";
+        if (!guessed2.contains(word2.charAt(0))) {
+            hintChar = word2.substring(0,1);
+            return "Starts with: " + hintChar;
+        } else {
+            for (int i = 1; i < word2.length(); i++) {
+                if (!guessed2.contains(word1.charAt(i))) {
+                    hintChar = word2.substring(i,i+1);
+                    break;
+                } 
+            }
+        }
+
+        return "Contains letter " + hintChar;
+    }
+    public String getWord1() {
+        return this.word1;
+    }
+    
+    public String getWord2() {
+        return this.word2;
+    }
+    
+    public String getWrongGuesses1String() {
         return wrongGuesses1.toString();
     }
    
-    public String getWrongGuesses2() {
+    public String getWrongGuesses2String() {
         return wrongGuesses2.toString();
+    }
+    
+    public LinkedList<Character> getWrongGuesses1() {
+        return wrongGuesses1;
+    }
+   
+    public LinkedList<Character> getWrongGuesses2() {
+        return wrongGuesses2;
     }
 
     public int getNumWrongGuesses1() {
@@ -157,12 +184,11 @@ public class MultiPlayerRound {
     public int getNumWrongGuesses2() {
         return wrongGuesses2.size();
     }
-    
+
     /**
      * Tests the class
      */
     public static void main(String[] args) {
-        MultiPlayerRound spr = new MultiPlayerRound();
-        spr.makeGuess(); 
+        //needs to be filled 
     }
 }
